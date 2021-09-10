8bbb5191b34c9b66ba3953c255e1a0c61de20981
Stackbit Code Editor <projects@stackbit.com>
2021-09-10 19:32:05 +0000

    basicPage.js: changed

--- src/templates/basicPage.js
+++ src/templates/basicPage.js
@@ -1,5 +1,9 @@
 import React from "react";
 import styles from "./basicPage.module.css";
+import {
+  fieldPath as fpath,
+  objectId as oid
+} from '@stackbit/annotations';
 
 export default function BasicPage(props) {
   return (
