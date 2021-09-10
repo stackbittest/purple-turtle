ef736b772e808203bab7c09066d4a3b9a4b95e0b
Stackbit Code Editor <projects@stackbit.com>
2021-09-10 19:36:36 +0000

    [[...rooturlslug]].js: changed

--- src/pages/[[...rooturlslug]].js
+++ src/pages/[[...rooturlslug]].js
@@ -30,6 +30,7 @@ export async function getStaticProps(context) {
   const doc = await getDocByFilePath(extraparams.params.originalfilepath);
   return {
     props: {
+      tokentopass,
       hello: params ? JSON.stringify(params) : "world",
       templatetype: extraparams.params.templatetype,
       ...doc,
