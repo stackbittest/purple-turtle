9562fbae9a6134901b901fe9b10ecee4cb399137
Stackbit Code Editor <projects@stackbit.com>
2021-09-10 19:44:17 +0000

    basicPage.js: changed

--- src/templates/basicPage.js
+++ src/templates/basicPage.js
@@ -6,6 +6,6 @@ export default function BasicPage(props) {
     <>
       <h1>A basic page called {props.hello}</h1>
       <div className={styles.testingcss} data-sb-field-path=".meta.message">I am basic, but I can tell you:  {props.meta.message}</div>
-    </div>
+    </>
   );
 }
