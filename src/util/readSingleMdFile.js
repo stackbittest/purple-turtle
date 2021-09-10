d6c3f9e7e7ecc25f8389e4042265e8613a3b6a77
Stackbit Code Editor <projects@stackbit.com>
2021-09-10 19:48:16 +0000

    readSingleMdFile.js: changed

--- src/util/readSingleMdFile.js
+++ src/util/readSingleMdFile.js
@@ -4,5 +4,5 @@ import { promises as fs } from "fs";
 export async function getDocByFilePath(filepath) {
   const fileContents = await fs.readFile(filepath, "utf8");
   const { data, content } = matter(fileContents);
-  return { meta: data, content };
+  return { filepath, meta: data, content };
 }
