0b5e8654987a5fca57798ac871f9929467266d64
Stackbit Code Editor <projects@stackbit.com>
2021-09-10 19:56:22 +0000

    [[...rooturlslug]].js: changed

--- src/pages/[[...rooturlslug]].js
+++ src/pages/[[...rooturlslug]].js
@@ -31,7 +31,7 @@ export async function getStaticProps(context) {
   const doc = await getDocByFilePath(extraparams.params.originalfilepath);
   return {
     props: {
-      originalfilepath: extraparams.params.originalfilepath.replace('/home/appuser/app/',''),
+      originalfilepath: extraparams.params.originalfilepath.replace('/home/appuser/app/src/content/',''),
       hello: params ? JSON.stringify(params) : "world",
       templatetype: extraparams.params.templatetype,
       ...doc,
